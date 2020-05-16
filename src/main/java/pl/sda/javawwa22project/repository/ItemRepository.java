8fe44e9452c98d6f9cd3fa458bd20ea5c91067c2
Robert Dzienio <robertdzienio@gmail.com>
2020-05-16 12:10:12 +0200

    [change] - refactor code

--- src/main/java/pl/sda/javawwa22project/repository/ItemRepository.java
+++ src/main/java/pl/sda/javawwa22project/repository/ItemRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sda.javawwa22project.entity.Item;
 
-@Repository
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
