6e9f42b2019e63ea891abe0398342bf612259a50
Robert Dzienio <robertdzienio@gmail.com>
2020-05-16 14:14:09 +0200

    [change] - ItemController

--- src/main/java/pl/sda/javawwa22project/controller/ItemController.java
+++ src/main/java/pl/sda/javawwa22project/controller/ItemController.java
@@ -1,15 +1,39 @@
 package pl.sda.javawwa22project.controller;
 
+import org.dom4j.rule.Mode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.sda.javawwa22project.converter.ItemConverter;
 import pl.sda.javawwa22project.entity.Item;
+import pl.sda.javawwa22project.service.ItemService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ItemController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemController.class);
+    private final ItemService itemService;
+    private final ItemConverter itemConverter;
+
+    public ItemController(final ItemService itemService, final ItemConverter itemConverter) {
+        this.itemService = itemService;
+        this.itemConverter = itemConverter;
+    }
+
+    @GetMapping("/items/{id}")
+    String getItemById(@PathVariable Long id, Model model) {
+        var itemDto = itemService.findById(id)
+                .map(itemConverter::fromItem)
+                .orElse(null);
+        model.addAttribute("item", itemDto);
+        return "";
+    }
 
     // TODO
     // metoda wyświetla 3 przykładowe przedmioty
