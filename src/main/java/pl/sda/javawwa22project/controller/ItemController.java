6950190f8b9ec9acf9e1fb908312cfdf3b473729
Robert Dzienio <robertdzienio@gmail.com>
2020-05-17 14:08:24 +0200

    [add] - add & save item endpoint

--- src/main/java/pl/sda/javawwa22project/controller/ItemController.java
+++ src/main/java/pl/sda/javawwa22project/controller/ItemController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.javawwa22project.converter.ItemConverter;
 import pl.sda.javawwa22project.dto.ItemDto;
 import pl.sda.javawwa22project.service.ItemService;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.stream.Collectors;
 
@@ -54,5 +56,18 @@ public class ItemController {
         LOGGER.info("Items found: {}", allItemsDto);
         model.addAttribute(MANY_ITEMS_KEY, allItemsDto);
         return "items/all-items";
+    }
+
+    @GetMapping("/add-item")
+    String addItem() {
+        LOGGER.info("addItem()");
+
+        return "/items/add-edit";
+    }
+
+    @PostMapping("/save-item")
+    String saveItem(@Valid ItemDto itemToSave) {
+
+        return "redirect://items" + itemToSave.getId();
     }
 }
