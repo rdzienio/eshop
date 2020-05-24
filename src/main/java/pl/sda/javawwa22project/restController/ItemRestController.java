406abe47359b23be0130d943dab5855659683852
Robert Dzienio <robertdzienio@gmail.com>
2020-05-24 12:42:37 +0200

    [change] - rest save endpoint

--- src/main/java/pl/sda/javawwa22project/restController/ItemRestController.java
+++ src/main/java/pl/sda/javawwa22project/restController/ItemRestController.java
@@ -2,11 +2,7 @@ package pl.sda.javawwa22project.restController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.javawwa22project.converter.ItemConverter;
 import pl.sda.javawwa22project.dto.ItemDto;
 import pl.sda.javawwa22project.service.ItemsService;
@@ -18,7 +14,6 @@ public class ItemRestController {
     private final ItemsService itemsService;
     private final ItemConverter itemConverter;
 
-    private static final String ONE_ITEM_KEY = "itemsToShow";
     private static final Logger logger = LoggerFactory.getLogger(ItemRestController.class);
 
     public ItemRestController(final ItemsService itemsService, final ItemConverter itemConverter) {
@@ -27,12 +22,20 @@ public class ItemRestController {
     }
 
     @GetMapping("/items/{id}")
-    public ItemDto displayItemById(@PathVariable Long id, Model model) {
+    public ItemDto displayItemById(@PathVariable Long id) {
         logger.info("displayItemById with id: [{}]", id);
         return itemsService.findItemById(id)
                 .map(itemConverter::fromItem)
                 .orElse(ItemDto.builder().build());
 
+    }
+
+    @PostMapping("/item-save")
+    public ItemDto saveItem(ItemDto itemToSave) {
+        logger.info("saveItem(), received param: [{}]", itemToSave);
+
+        var item = itemConverter.fromDto(itemToSave);
+        return itemConverter.fromItem(itemsService.saveItem(item));
     }
 
 }
